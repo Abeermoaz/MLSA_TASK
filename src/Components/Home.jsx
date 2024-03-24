b582a55e34d190f4f85a79c664d1196e8626959d
Abeermoaz <18994@skylineuniversity.ac.ae>
2024-03-24 23:25:32 +0400

    added second image in the exisiting image and changed the image

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -2,16 +2,16 @@ import React from "react";
 import arrowSvg from "../images/down-arrow.svg";
 import PropTypes from "prop-types";
 import image from "../images/image1.png"; // Main image
-import secondaryImage from "../images/new.jpg"; // Secondary image
+import secondaryImage from "../images/new.jpg"; // Small circular image
 
 const imageAltText = "Adult female in office setting leaning against a glass wall while holding a platinum Microsoft Surface Pro 7 in tablet mode preparing to write with Microsoft Surface Pen";
 
 const Home = ({ name, title }) => {
   return (
     <section id="home" className="min-height">
-      <div style={{ position: "relative" }}>
+      <div style={{ position: "relative", display: "inline-block" }}>
         <img className="background" src={image} alt="" />
-        <img className="secondary-image" src={secondaryImage} style={{ position: "absolute", top: "0", right: "0", height: "100%", width: "auto" }} alt="" />
+        <img className="secondary-image" src={secondaryImage} style={{ position: "absolute", top: "5rem", right: "2rem", height: "4rem", width: "4rem", borderRadius: "50%" }} alt="" />
       </div>
       <div style={{ position: "absolute", top: "5rem", left: "2rem", width: "17rem" }}>
         <h1>{name}</h1>
