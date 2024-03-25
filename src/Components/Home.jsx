b4dd1ac96450f52ed6b2c74abb082dd23a9e95de
Abeermoaz <18994@skylineuniversity.ac.ae>
2024-03-25 19:51:53 +0400

    added a new image in the about

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -15,7 +15,7 @@ import PropTypes from "prop-types";
  */
 import image from "../images/web.png";
 
-const imageAltText = "Adult female in office setting leaning against a glass wall while holding a platinum Microsoft Surface Pro 7 in tablet mode preparing to write with Microsoft Surface Pen";
+const imageAltText = "A green aesthetic background image ";
 
 const Home = ({ name, title }) => {
   return (
